b14d4358abb8e0df5cee74b6529a4bb6fa595c9e
jsevern94 <severnj@spu.edu>
2019-06-11 03:19:14 -0700

    animations improved and updated to work in reverse, better scrolling functionality

--- app.js
+++ app.js
@@ -1,62 +1,31 @@
-$(document).ready(function () {
-    $("#scroll-button").click(function () {
-        $('html,body').animate({
-            scrollTop: $("#section2").offset().top
-        },
-            'slow');
+$(window).scroll(function () {
+    if ($("#scroll-button").is(':in-viewport')) {
+        $("#side-nav").removeClass("slide-in-blurred-top");
+        $("#side-nav").addClass("slide-out-blurred-top");
+    } else {
+        $("#side-nav").removeClass("slide-out-blurred-top");
         $("#side-nav").addClass("slide-in-blurred-top");
         $("#side-nav").removeClass("invisible");
+    }
+    $(".lefty").each(function (i, el) {
+        var el = $(el);
+        if (el.is(':in-viewport')) {
+            el.addClass("slide-in-blurred-left");
+            el.removeClass("invisible")
+        } else {
+            el.removeClass("slide-in-blurred-left")
+            el.addClass("invisible")
+        }
+    });
+
+    $(".righty").each(function (i, el) {
+        var el = $(el);
+        if (el.is(':in-viewport')) {
+            el.addClass("slide-in-blurred-right");
+            el.removeClass("invisible")
+        } else {
+            el.removeClass("slide-in-blurred-right")
+            el.addClass("invisible")
+        }
     });
 });
-
-
-
-
-(function($) {
-
-    /**
-     * Copyright 2012, Digital Fusion
-     * Licensed under the MIT license.
-     * http://teamdf.com/jquery-plugins/license/
-     *
-     * @author Sam Sehnert
-     * @desc A small plugin that checks whether elements are within
-     *     the user visible viewport of a web browser.
-     *     only accounts for vertical position, not horizontal.
-     */
-  
-    $.fn.visible = function(partial) {
-      
-        var $t            = $(this),
-            $w            = $(window),
-            viewTop       = $w.scrollTop(),
-            viewBottom    = viewTop + $w.height(),
-            _top          = $t.offset().top,
-            _bottom       = _top + $t.height(),
-            compareTop    = partial === true ? _bottom : _top,
-            compareBottom = partial === true ? _top : _bottom;
-      
-      return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
-  
-    };
-      
-  })(jQuery);
-
-
-  $(window).scroll(function(event) {
-  
-    $(".lefty").each(function(i, el) {
-      var el = $(el);
-      if (el.visible(true)) {
-        el.addClass("slide-in-blurred-left"); 
-      } 
-    });
-
-    $(".righty").each(function(i, el) {
-        var el = $(el);
-        if (el.visible(true)) {
-          el.addClass("slide-in-blurred-right"); 
-        } 
-      });
-    
-  });
