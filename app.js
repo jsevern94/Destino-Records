83d6ce87102edf83d7fb8781a1f1e4a71a6f7491
jsevern94 <severnj@spu.edu>
2019-06-11 05:17:14 -0700

    tentatvely adding an animation to text to make it more visible against background

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ $(window).scroll(function () {
     if ($("#scroll-button").is(':in-viewport')) {
         $("#side-nav").removeClass("slide-in-blurred-top");
         $("#side-nav").addClass("slide-out-blurred-top");
+        $(".bio-text").removeClass("text-pop-up-top")
     } else {
         $("#side-nav").removeClass("slide-out-blurred-top");
         $("#side-nav").addClass("slide-in-blurred-top");
         $("#side-nav").removeClass("invisible");
+        $(".bio-text").addClass("text-pop-up-top")
     }
     $(".lefty").each(function (i, el) {
         var el = $(el);
@@ -28,4 +30,5 @@ $(window).scroll(function () {
             el.addClass("invisible")
         }
     });
+
 });
